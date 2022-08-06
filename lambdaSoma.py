895b6737a2743cf86c5ea060fffcef0c45fab73d
César Henrique Alves Oncala <52250904+CesarOncala@users.noreply.github.com>
2022-08-06 11:53:04 -0300

    Add files via upload

---
+++ lambdaSoma.py
@@ -0,0 +1,17 @@
+soma = lambda x,y: x+y
+
+print(soma(2,4))
+
+-----------------------
+list comprehension:
+
+[expression for item in list (optional ->  if .... )]
+
+-----------------------
+
+enumerate() => retorna uma lista de tupla de uma lista,  em que cada elemento x tem um indice y
+
+-----------------------
+
+
+try except finnaly
